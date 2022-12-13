28620989821a5eb4afce91cb2e63b46db5a9154f
Weng-john <59682713+Weng-john@users.noreply.github.com>
2022-12-13 19:55:13 +0800

    tsai course

--- toeplit matrix.c
+++ toeplit matrix.c
@@ -10,6 +10,8 @@ int main()
         int n;
         printf("Enter n: ");
         while(scanf("%d", &n)!=EOF){
+                int LR= 0, RL= 0;
+
                 printf("Enter num: ");
                 for(int i=0;i<n;i++)
                         scanf("%d", num+i);
@@ -18,9 +20,21 @@ int main()
                         if(i)
                                 for(int j=1;j<n;j++)
                                         arr[i][j]= arr[i-1][j-1];
-                        else
-                                for(int j=0;j<n;j++)
-                                        arr[i][j]= arr[j][i]= j+1;
+                        else{
+                                for(int j=0;j<n;j++){
+                                        arr[j][i]= num[j];
+                                        arr[i][j]= num[j];
+                                }
+                        }
+                }
+
+                for(int i=0;i<n;i++){
+                        for(int j=0;j<n;j++){
+                                if((i+j)==(n-1))
+                                        RL += arr[i][j];
+                                if(i==j)
+                                        LR += arr[i][j];
+                        }
                 }
 
                 printf("\nThe Matrix:\n");
@@ -29,6 +43,9 @@ int main()
                                 printf("   %d", arr[i][j]);
                         printf("\n");
                 }
+                printf("Diagonal sum \:  %d\n", LR);
+                printf("Diagonal sum /:  %d\n", RL);
+
                 printf("Enter n: ");
         }
         return 0;
